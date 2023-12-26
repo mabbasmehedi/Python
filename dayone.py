3208bbf9c2242dca57a7fc796b63d61bbeb9d109
MeheDumb <mehedi2205101523@diu.edu.bd>
2023-12-26 22:08:20 +0600

    upadated

---
+++ dayone.py
@@ -0,0 +1,17 @@
+#syntext, comment,
+print("Hello World")
+
+if 5>2:
+ print("5 is gratter then 2")
+ print("Uporer line er shoman space dite hbe")
+ 
+ #demo comment
+
+ #line 1 of multiline
+ #line 2 of multiline
+
+ """use a multiline string.
+ for multiline commet"""
+
+
+
---
+++ variable.py
@@ -0,0 +1,55 @@
+x = 4
+y = "Salim"
+z = 'Kolim' #String e single ar double qt same
+print(x)
+print(y)
+print(z)
+
+a = str(3)
+b = int(3)  
+c = float(3)
+print(a)
+print(b)
+print(c)  
+
+print(type(y))
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Case sencetive akta arekta re overrite korbe na
+a = 4
+A = "Sally"
+
+""" variable name contain:
+A-Z, a-z, 0-9 and _
+cannot start with a letter, and it case sensitive
+Okey:
+ _my_var = "John"
+myVar = "John"
+not okey:
+2myvar = "John"
+my-var = "John" 
+"""
+
+#asign multiiple value in one line
+x2, y2, z2 = "Orange", "Banana", "Kola"
+#assign single value to multiple variable
+x3 = y3 = z3 = "Bangi"
+#Unpack collection
+fruits = ["apple", "banana", "cherry"]
+x4, y4, z4 = fruits
+#multple variable output
+print(x4, y4, z4) 
+print(x2 + y2 +z2) #result e kono space ashbe na
+
+x5 = 5
+y5 = 10
+print(x5 + y5)
+
+x6 = 5
+y6 = "John"
+print(x6, y6)
+
+
+
