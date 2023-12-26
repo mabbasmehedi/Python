56f8b5fa3d7c34b81e0604fb5202d7f862a26b36
MeheDumb <mehedi2205101523@diu.edu.bd>
2023-12-26 15:01:55 +0600

    previous

---
+++ max.py
@@ -0,0 +1,3 @@
+number=[2, 5, 8,7 ,7,6,4]
+Maximum = max(number)
+print("Maximun number is: ", Maximum)
---
+++ min.py
@@ -0,0 +1,3 @@
+Numbers=[7,5,8,1,9,7,6]
+Minimum= min(Numbers)
+print("Min nuumber is:",Minimum )
