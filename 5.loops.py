b4440f2d30ea9da5035a3f0f470a245a4007c6b9
MeheDumb <mehedi2205101523@diu.edu.bd>
2024-11-08 08:28:26 +0600

    Add Prb Solving

---
+++ 5.loops.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    while True:
+        n = int(input())
+        if 1<= n <=20:
+            break
+    
+    for i in range(n):
+        print(i**2)
---
+++ 7.PrintFunc.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    while True:
+        if 1<=n<=150:
+            break
+            
+    for i in range(1,n+1):
+        print(i, end="")
